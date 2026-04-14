d9b230a5844f0f6f2648dd10f25b1d22ea185169
BILAL KHAIDOUME <bkhaidoume@gmail.com>
2026-04-14 15:37:40 +0100

    created structure.sql

---
+++ structure.sql
@@ -0,0 +1,18 @@
+USE bilal ;
+CREATE Table students (
+    id int AUTO_INCREMENT PRIMARY KEY ,
+    nameg VARCHAR(20),
+    lastb VARCHAR(20)
+);
+
+INSERT INTO students (
+    nameg,lastb
+)
+VALUES (
+    "bil&l",
+    "simo"
+)
+
+SELECT * FROM students
+
+DROP TABLE bilal ;
