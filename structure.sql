1010fa899dae70370bb59b4f44e1757682718223
BILAL KHAIDOUME <bkhaidoume@gmail.com>
2026-04-16 11:39:03 +0100

    connected vs code to the database

--- structure.sql
+++ structure.sql
@@ -1,18 +1,47 @@
-USE bilal ;
-CREATE Table students (
-    id int AUTO_INCREMENT PRIMARY KEY ,
-    nameg VARCHAR(20),
-    lastb VARCHAR(20)
+-- Active: 1776332007246@@127.0.0.1@3306@edusync
+CREATE DATABASE edusync ;
+USE edusync ;
+
+CREATE TABLE roles (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    label VARCHAR(20)
 );
 
-INSERT INTO students (
-    nameg,lastb
-)
-VALUES (
-    "bil&l",
-    "simo"
-)
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    firstname VARCHAR(20) NOT NULL,
+    lastname VARCHAR(20) NOT NULL,
+    email VARCHAR(50) UNIQUE NOT NULL,
+    password VARCHAR(256) NOT NULL,
+    roles_id INT UNIQUE ,
+    FOREIGN KEY (roles_id) REFERENCES roles(id)
+);
 
-SELECT * FROM students
+CREATE TABLE classes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    classrom_number INT NOT NULL
+);
 
-DROP TABLE bilal ;
+CREATE TABLE courses (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(50) NOT NULL,
+    description VARCHAR(50) NOT NULL,
+    total_hours INT NOT NULL
+);
+
+CREATE TABLE students (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    date_of_birth DATE NOT NULL,
+    student_number INT NOT NULL,
+    classes_id INT,
+    FOREIGN KEY (classes_id) REFERENCES classes(id),
+    users_id INT,
+    FOREIGN KEY (users_id) REFERENCES users(id)
+);
+
+CREATE TABLE enrollments (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    enrolled_at DATE NOT NULL,
+    status 
+)
